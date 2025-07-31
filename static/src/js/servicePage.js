17264eccff2a117063162ab0249df367d1d77594
Mureed Sultan <mureedsultangeni@gmail.com>
2025-07-31 08:17:00 +0500

    test upload

--- static/src/js/servicePage.js
+++ static/src/js/servicePage.js
@@ -1051,11 +1051,11 @@ function createServiceBlock(service) {
         <div class="_4block-faq-text-div-main ogg-font">
             <div class="_4block-top-text-div">
                 <div class="_4block-faq-text-top-div">
-                    <h1 class="p-30 service-gap">${service.name}<span>${service.arabic || ""}</span></h1>
+                    <h1 class="p-30 service-gap">${service.name} <span>${service.arabic||""}</span></h1>
                 </div>
             </div>
             <div class="_4block-text-bottom-main-div">
-                <div class="_4block-faq-text-bottom-div"><p class="service-gap font-900">Started From <span>AED
+                <div class="_4block-faq-text-bottom-div"><p class="service-gap font-900">Price  <span>AED
                 ${service
                 .price}</span></p></div>
                 <a   href="/booking/${service.name.replace(' ', '-')}"  class="_4block-btn-mob
